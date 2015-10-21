86d93046ddff39456861817741bd39fbb6f84755
albertoroque <alberto.roque097@hotmail.com>
2015-10-21 00:31:14 -0200

    primeiro mapeamento do bd #3

---
+++ docs/bd/inserts.sql
@@ -0,0 +1,4 @@
+INSERT INTO `ponteverde`.`pais` (`id`, `nome`) VALUES ('1', 'Brasil');
+INSERT INTO `ponteverde`.`pais` (`id`, `nome`) VALUES ('2', 'United States of America');
+INSERT INTO `ponteverde`.`pais` (`id`, `nome`) VALUES ('3', 'Argentina');
+INSERT INTO `ponteverde`.`pais` (`id`, `nome`) VALUES ('4', 'Portugal');
