4cf9809b358d13abe8846cadc1b3418da3052323
albertoroque <alberto.roque097@hotmail.com>
2015-11-19 17:00:46 -0200

    começo da implementação do controle de troca de fotos dos perfis

---
+++ ponteverde/ponteverde/Controllers/FotoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ponteverde.Controllers
+{
+    public class FotoController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
